7c03b1fd9f1432acedb33507767360f7eec2081a
krishna kumar <krishnakumarjms12@gmail.com>
2023-02-18 18:52:13 +0530

    some changes in backend

--- app/src/main/java/com/example/note/utils/Constants.kt
+++ app/src/main/java/com/example/note/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.example.note.utils
 //recommended practice for declaring constants...
 object Constants {
     const val  TAG = "KRISHNA"
-    const val BASE_URL = "https://krishnanoteapi.herokuapp.com"
+    const val BASE_URL = "https://nutty-culottes-eel.cyclic.app/"
     const val PREFS_TOKEN_FILE = "PREFS_TOKEN_FILE"
     const val USER_TOKEN = "USER_TOKEN"
 
